406c580ec6c6b4ba1b2bd886a32a612eabc163dc
Ubuntu <ubuntu@ip-172-31-29-23.ap-southeast-1.compute.internal>
2019-06-29 14:43:03 +0000

    Added 2nd time deployment condition

--- deploy_in_aws.sh
+++ deploy_in_aws.sh
@@ -13,20 +13,29 @@ mkdir -p ~/$appName"-directory"
 
 read -p "Enter git repository name! " repoName
 
-mkdir ~/repositories/$repoName
 cd ~/repositories/
 
-read -p "Enter git Url! " gitUrl
-git clone $gitUrl
-if [ $? -ne 0 ] ;then
- echo "Problem in cloning the git! Check the creadentials or Url "
- exit 1
-fi
+is_repo_already_exists=`ls | grep -c $repoName`
+
+if [ $is_repo_already_exists = 0 ] ;then
+	read -p "Enter git Url! " gitUrl
+	git clone $gitUrl
+	if [ $? -ne 0 ] ;then
+	  echo "Problem in cloning the git! Check the creadentials or Url "
+	  exit 1
+	fi
+else
+	cd ~/repositories/$repoName
+	git pull
+        if [ $? -ne 0 ] ;then
+          echo "Problem in pulling the latest code! Check the creadentials !! "
+          exit 1
+        fi
+fi	
 
 cd ~/repositories/$repoName
 read -p "Enter git branch that to be deployed! " gitBranch
 git checkout $gitBranch
-
 if [ $? -ne 0 ] ;then
  echo "Problem in checking out the git branch ! Check if the branch exists in git "
  exit 1
@@ -35,7 +44,7 @@ fi
 cp ~/repositories/$repoName/* ~/$appName"-directory/"
 cd ~/$appName"-directory"
 
-read -p "Press 1 if it is 1st time deployment! else 0 !!" is_first_deployment
+read -p "Press 1 if it is 1st time deployment! else 0 !! " is_first_deployment
 if [ $is_first_deployment = 1 ] ;then
  eb init
  eb create
@@ -43,4 +52,6 @@ fi
 
 eb deploy
 
+url=`eb status | grep CNAME: | cut -d ':' -f2 | awk '{$1=$1};1'`
 
+echo " And Thats it!! Navigate to the following URL : " $url
